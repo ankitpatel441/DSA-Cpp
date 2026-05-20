2a32b7d64a9632c7c4eb6d2bc9e69d249bc345e7
ankitpatel441 <ankitpatel441020@gmail.com>
2026-05-20 23:20:01 +0530

    Add new files for pointer questions and update code examples in the Introduction to Pointers section

--- 00_cpp_Fundamentals/08_Introduction_to_Pointers/code.cpp
+++ 00_cpp_Fundamentals/08_Introduction_to_Pointers/code.cpp
@@ -19,6 +19,30 @@ Topics Covered:
 #include <iostream>
 using namespace std;
 
+// // pass by value
+// void changeA(int a) {
+//     a = 20;
+//     cout << a << "\n";
+// }
+
+// // pass by reference using pointer
+// void changeB(int *ptr) {
+//     *ptr = 20;
+//     cout << *ptr << "\n";
+// }
+
+// // pass by reference using pointer
+// void changeB(int *ptr) {
+//     *ptr = 20;
+//     cout << *ptr << "\n";
+// }
+
+// pass by reference using reference variables
+void changeA(int &a) {
+    a = 20;
+    cout << a << "\n";
+}
+
 int main() {
 
     // // Basic pointer
@@ -60,11 +84,24 @@ int main() {
     //   cout << ptr << endl;
     //   cout << *ptr << endl;
       
-      //NUll  pointer
-      int *ptr =  NULL;
-      cout << ptr << endl;
-      cout << *ptr << endl; //segmentation error
+    //   //NUll  pointer
+    //   int *ptr =  NULL;
+    //   cout << ptr << endl;
+    //   cout << *ptr << endl; //segmentation error
 
+    // int a = 10;
+    // int b = 20;
+
+    // changeA(a);
+    // changeB(&b);
+
+    // cout << "A: " << a << endl;
+    // cout << "B: " << b << endl;
+
+    int a = 10;
+    // int &b; // error
+    changeA(a);
+    cout << a << endl;
 
     return 0;
 }
---
+++ 00_cpp_Fundamentals/08_Introduction_to_Pointers/pqs.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+// Qs. What will ptr2 point to in the following code:
+// int x = 5, y = 10;
+// int *ptr1 = &x, *ptr2 = &y;
+// ptr2 = ptr1;
+
+// Qs. What is the type of a & b :
+// float* a, b;
+// a - float ptr
+// b - normal  float
+
+//Qs. Can we initialize a pointer with value 0 ? 
+
+int main() {
+// int x = 5, y = 10;
+// int *ptr1 = &x, *ptr2 = &y;
+// ptr2 = ptr1;
+
+// cout << ptr2 << endl;
+// cout << ptr1 << endl;
+// cout << &x << endl;
+
+// float* a, b;
+
+// int *ptr = 0;
+// cout << ptr << endl;
+
+
+    return 0;
+}
