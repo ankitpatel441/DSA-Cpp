34c73e35afe3a91399099a05e47c35aa754e459c
ankitpatel441 <ankitpatel441020@gmail.com>
2026-05-29 23:53:38 +0530

    Fix Trapping Rain Water algorithm implementation and correct function name

--- 01_Arrays/TrappingRainWater.cpp
+++ 01_Arrays/TrappingRainWater.cpp
@@ -2,30 +2,37 @@
 #include <climits>
 using namespace std;
 
-void teap(int *height, int n){
-    int leftMax[20000], rightMax [20000];
-   leftMax[0] = INT_MIN;
-   rightMax[0] = INT_MIN;
-   
-   for(int i = 1; i<n; i++) {
-    leftMax[i] = max(leftMax[i-1], height[i-1]);
-   }
+void trap(int *height, int n) {
+    int leftMax[20000], rightMax[20000];
 
-   for(int i= n-2; i>=0; i--){
-    rightMax[i] = max(rightMax[i+1], rightMax[i+1]);
-   }
+    leftMax[0] = INT_MIN;
+    for (int i = 1; i < n; i++) {
+        leftMax[i] = max(leftMax[i - 1], height[i - 1]);
+    }
 
-   int waterTeaped = 0;
-   for(int i)
-   cout << endl;
+    rightMax[n - 1] = INT_MIN;
+    for (int i = n - 2; i >= 0; i--) {
+        rightMax[i] = max(rightMax[i + 1], height[i + 1]);
+    }
+
+    int waterTrapped = 0;
+
+    for (int i = 1; i < n - 1; i++) {
+        int waterLevel = min(leftMax[i], rightMax[i]);
+
+        if (waterLevel > height[i]) {
+            waterTrapped += waterLevel - height[i];
+        }
+    }
+
+    cout << "Total Water Trapped = " << waterTrapped << endl;
 }
 
-int main () {
+int main() {
     int height[] = {4, 2, 0, 6, 3, 2, 5};
     int n = sizeof(height) / sizeof(int);
 
-    teap(height, n);
-
+    trap(height, n);
 
     return 0;
 }
